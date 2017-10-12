e7dbd8d2122f124878326bdafe1119fe96cd842e
HashedDan <dmangum@hashedhealth.com>
2017-10-12 15:04:03 -0500

    basic structure

---
+++ blockchain.py
@@ -0,0 +1,24 @@
+
+
+class Blockchain(object):
+	def __init__(self):
+		self.chain = []
+		self.current_transactions = []
+
+	def new_block(self):
+		# TODO
+		pass
+
+	def new_transaction(self):
+		# TODO
+		pass
+
+	@staticmethod
+	def hash(block):
+		# TODO
+		pass
+
+	@property
+	def last_block(self):
+		# TODO
+		pass
